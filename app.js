1f1ea6af11a28db3cca4e07025c5a63e602a1e29
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-05-17 22:04:03 -0500

    Fuera de tiempo pero terminado :D

--- app.js
+++ app.js
@@ -30,17 +30,18 @@ exports.agregarTarea = function (tarea) {
         const archivo = mod_file.readFileSync('tareas.json','utf-8'); 
         
         let stringfy;
-        if (archivo.length > 0) {
-            const tareas = JSON.parse(archivo);
-            stringfy = JSON.stringify(tareas);
-            //stringfy = stringfy.slice(0,stringfy.length-1);
-            console.log(stringfy);
-            stringfy = stringfy + "," + JSON.stringify(tarea);    
+        const tareas = JSON.parse(archivo);
+
+        if (tareas.length > 0) {
+            
+            stringfy= JSON.stringify(tareas);
+            stringfy = stringfy.slice(0,stringfy.length-1);
+            stringfy = stringfy + "," + JSON.stringify(tarea) + "]";
         } else {
-            stringfy = JSON.stringify(tarea);    
+            stringfy = "["+JSON.stringify(tarea)+"]";    
         }
         
-        mod_file.writeFile('tareas.json', stringfy, function (err) {
+        mod_file.writeFileSync('tareas.json', stringfy, function (err) {
             if (err) return false;
         });
         return true;
@@ -55,8 +56,14 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
-   const tareas = this.leerTareas();
-   const tareasFiltradas = tareas.filter(tarea => tarea.status === estado);
-   return tareasFiltradas;
+        const mod_file = require('fs');
+        try {
+            const archivo = mod_file.readFileSync('tareas.json','utf-8');    
+            const tareasFiltradas = JSON.parse(archivo).filter(tarea => tarea.status === estado);
+            return tareasFiltradas;
+        } catch (error) {
+            return null;
+        }
+   
 
 }
